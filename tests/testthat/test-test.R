3fa52d52b1078fb5aac12e29335580c089c43519
colmds-localadmin <colmds-localadmin@UOB192055.adf.bham.ac.uk>
2021-05-11 13:39:59 +0100

    Fix test file

--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -1,17 +1,17 @@
 test_that("data sim works.",{
   #.libPaths("C:/docs/R")
-  suppressWarnings(require(sp))
+  #suppressWarnings(require(sp))
   square <- data.frame(x=c(0,0,1,1),y=c(0,1,1,0))
-  square <- SpatialPolygons(list(Polygons(list(Polygon(square)),"s1")))
+  square <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(square)),"s1")))
   square <- as(square,"SpatialPolygonsDataFrame")
   square_pop <- expand.grid(x=seq(0,1,by=0.1),y=seq(0,1,by=0.1))
-  square_pop <- SpatialPointsDataFrame(coords= square_pop, data=square_pop)
-  gridded(square_pop) <- TRUE
+  square_pop <- sp::SpatialPointsDataFrame(coords= square_pop, data=square_pop)
+  sp::gridded(square_pop) <- TRUE
   square_pop <- as(square_pop,"SpatialPolygons")
   square_pop <- as(square_pop,"SpatialPolygonsDataFrame")
   square_pop@data$popdens <- abs(rnorm(nrow(square_pop@data)))
 
-  dat <- infecSim(region = square,
+  dat <- realTimeSurv::infecSim(region = square,
                   t.win = c(1,7),
                   covariates = square_pop,
                   mean.val= 10,
@@ -24,13 +24,13 @@ test_that("data sim works.",{
 
   expect_s3_class(dat,"data.frame")
 
-  mintest <- mincontrast_st(dat,
+  mintest <- realTimeSurv::mincontrast_st(dat,
                             square_pop,
                             square)
 
   expect_type(mintest,"double")
 
-  lg1 <- lgcp(data=dat,
+  lg1 <- realTimeSurv::lgcp(data=dat,
               pop.var = c("popdens"),
               boundary=square,
               covariates=square_pop,
@@ -53,7 +53,7 @@ test_that("data sim works.",{
   expect_s3_class(p1,"lgcpRealPlot")
   expect_s3_class(p1[[1]],"gg")
 
-  p2 <- plot_hotspot(lg1,
+  p2 <- realTimeSurv::plot_hotspot(lg1,
                      covariates = square_pop,
                      threshold.var = c("poppp+obs+latent",
                                        "poppp+obs+latent+lag(3)"),
